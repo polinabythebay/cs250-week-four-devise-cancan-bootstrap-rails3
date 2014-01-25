193c6a1209fe4cda2d134b67f114e111b3ca67d6
Polina Soshnin <psoshnin@wellesley.edu>
2014-01-24 21:09:40 -0800

    edit admin page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,9 @@
 
 
 class UsersController < ApplicationController
-
-	#this uses devise to make sure user is logged in
   before_filter :authenticate_user!
 
   def index
-
-  	#makes sure we limit access to page
-
-  	#cancan authorize! method will check the CanCan Ability class def to determine if the user can has the permission to access the index method
-  	
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
   end
@@ -19,4 +12,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+
+  def destroy
+    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+    user = User.find(params[:id])
+    unless user == current_user
+      user.destroy
+      redirect_to users_path, :notice => "User deleted."
+    else
+      redirect_to users_path, :notice => "Can't delete yourself."
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   	devise :database_authenticatable, :registerable,
          	:recoverable, :rememberable, :trackable, :validatable
 
+
+    # Setup accessible (or protected) attributes for your model
+    attr_accessible :role_ids, :as => :admin
+
   	# Setup accessible (or protected) attributes for your model
   	attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   	# attr_accessible :title, :body
@@ -19,3 +23,6 @@ class User < ActiveRecord::Base
   	validates_presence_of :name
 	validates_uniqueness_of :name, :email, :case_sensitive => false
 end
+
+
+

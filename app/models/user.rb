e03b6e8034f35644d1b37bbcc416d09fe16857c7
Polina Soshnin <psoshnin@wellesley.edu>
2014-01-24 16:44:50 -0800

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ActiveRecord::Base
-  rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  	rolify
+  	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, :registerable,
+         	:recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  	# Setup accessible (or protected) attributes for your model
+  	attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  	# attr_accessible :title, :body
+
+  	#validate the presence and uniqueness of the name variable
+  	#allows users to be created or edited with a name attribute
+  	#when a user is created, a name and email address must be present
+  	#and must be unique (not used before)
+  	#devise will check that the email address and password are not blank
+  	#and that the email address is unique
+  	
+  	validates_presence_of :name
+	validates_uniqueness_of :name, :email, :case_sensitive => false
 end
